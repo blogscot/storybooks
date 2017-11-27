4df3179fbf390d50cb32035b1010bb6f22d8f569
iain <iain.diamond@gmail.com>
2017-11-27 19:41:30 +0000

    Create user model

--- models/index.js
+++ models/index.js
@@ -1,10 +1,14 @@
 const mongoose = require('mongoose')
 const mongo = require('../config/database')
 
-mongoose.set('debug', true)
+const debug = process.env.NODE_ENV !== 'production'
+
+mongoose.set('debug', debug)
 mongoose.Promise = global.Promise
 mongoose.connect(mongo.URI, {
   keepAlive: true,
   reconnectTries: Number.MAX_VALUE,
   useMongoClient: true,
 })
+
+module.exports.User = require('./user')
