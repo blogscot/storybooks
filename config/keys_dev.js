35184f29010f813e2ed2721a62d56bdc511126f7
iain <iain.diamond@gmail.com>
2017-12-02 14:47:13 +0000

    Add keys

---
+++ config/keys_dev.js
@@ -0,0 +1,9 @@
+const keys = {
+  mongoURI:
+    'mongodb://blogscot:thisisnotmypassword@ds123146.mlab.com:23146/storybooks',
+  googleClientID:
+    '153477752947-2ir2cdac6fstan8rm3p2jig9huicj73c.apps.googleusercontent.com',
+  googleClientSecret: 'Bdu-2S101YcqCmpqhf4X2Ozf',
+}
+
+module.exports = keys
